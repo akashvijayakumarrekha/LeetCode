e72033e1aa4a40cc453c57eab0e29c8cc7de156d
Akash Vijayakumar Rekha <141734126+akashvijayakumarrekha@users.noreply.github.com>
2023-08-15 13:02:16 +0200

    Two Sums

--- Two Sum.py
+++ Two Sum.py
@@ -5,10 +5,7 @@ You may assume that each input would have exactly one solution, and you may not 
 
 You can return the answer in any order.
 
- 
-
 Example 1:
-
 Input: nums = [2,7,11,15], target = 9
 Output: [0,1]
 Explanation: Because nums[0] + nums[1] == 9, we return [0, 1].
@@ -32,7 +29,21 @@ Only one valid answer exists.
 
 
 def two_sum(num, target):
-    for i in (0, len(num)):
-        for j in (i + 1, len(num)):
+    for i in range(len(num)):
+        for j in range(i + 1, len(num)):
             if num[i] + num[j] == target:
                 return i, j
+
+
+# Get input from the user and split it into a list
+input_string = input("Enter elements of the list separated by spaces or commas: ")
+
+# Split the input_string into a list using either spaces or commas as separators
+input_list = input_string.split()  # This creates a list of strings
+
+# If you want to convert the input elements to integers
+input_list = [int(item) for item in input_list]
+
+target = int(input("Enter the target: "))
+
+print(two_sum(input_list, target))
